6b3c57818d99a564710ac11bdc06e873207a6d9c
ledford30 <101572791+Vesuro30@users.noreply.github.com>
2022-08-03 20:29:33 -0600

    test

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
+const entryRoute = require("./entryRoute");
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
+router.use("/", entryRoute);
 
 module.exports = router;
