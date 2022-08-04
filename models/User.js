2b03c24fc13ec7cfb9879c9ae6b613b2fba2c35d
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-08-04 16:29:31 -0600

    adding bcrypt & a beforeCreate hook

--- models/User.js
+++ models/User.js
@@ -1,20 +1,31 @@
 const { Model, DataTypes } = require("sequelize");
-// const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
     id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.UUID,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      unique: true,
     },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+      validate: {
+        isAlphanumeric: true,
+        notNull: {
+          msg: "Please enter your name",
+        },
+      },
     },
     email: {
       type: DataTypes.STRING,
@@ -33,6 +44,19 @@ User.init(
     },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
