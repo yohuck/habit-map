d613433a91b3ccc883122c85d919b32b7575426d
Eric Lake <99624222+yohuck@users.noreply.github.com>
2022-08-03 20:41:03 -0600

    Update seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -4,7 +4,7 @@ const { User } = require("../models");
 const userData = require("./userData.json");
 
 const seedDatabase = async () => {
-  await sequelize.sync({ fore: true });
+  await sequelize.sync({ force: true });
 
   const users = await User.bulkCreate(userData);
 
