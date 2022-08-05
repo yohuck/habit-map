0311497dd548e50af933f79a36cdc3e6a2a36cba
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-08-04 22:43:15 -0600

    added a console.log

--- seeds/seed.js
+++ seeds/seed.js
@@ -5,6 +5,7 @@ const userData = require("./userData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+  console.log("All models were synchronized successfully.");
 
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
