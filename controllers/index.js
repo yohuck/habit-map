777cdeea47f5c1a8d2582b4b9e034f3e6db08e54
ledford30 <101572791+Vesuro30@users.noreply.github.com>
2022-08-06 11:42:15 -0600

    small syntax fix

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
-const entryRoutes = require("./api/entryRoutes");
+// const entryRoutes = require("./api/entryRoutes");
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
-router.use("/entries", entryRoutes);
+// router.use("/entries", entryRoutes);
 
 module.exports = router;
