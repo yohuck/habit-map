d2a47e3f85f65cf6b0a22dbabe5c5c21e8a1dd94
ledford30 <101572791+Vesuro30@users.noreply.github.com>
2022-08-06 10:47:47 -0600

    small fix (model)

--- controllers/entryRoute.js
+++ controllers/entryRoute.js
@@ -1,6 +1,5 @@
-const Entry = require("../models/Entry");
-
 const router = require("express").Router();
+const { Entry } = require("../../models/");
 
 router.get("/", async (req, res) => {
   try {
