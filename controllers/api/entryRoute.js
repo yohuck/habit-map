e153c0d5cb177b4bcfac4d155c16a0439d4a9e88
ledford30 <101572791+Vesuro30@users.noreply.github.com>
2022-08-06 11:24:31 -0600

    moved entry into api folder

---
+++ controllers/api/entryRoute.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const { Entry } = require("../../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const entryData = await Entry.findAll(req.body);
+    res.status(200).json(habitData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+module.exports = router;
