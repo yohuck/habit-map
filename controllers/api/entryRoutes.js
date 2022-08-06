6d05e05a4811959c46dc85b51efe30890a331727
ledford30 <101572791+Vesuro30@users.noreply.github.com>
2022-08-06 13:00:30 -0600

    small fixes

--- controllers/api/entryRoutes.js
+++ controllers/api/entryRoutes.js
@@ -4,11 +4,13 @@ const { Entry } = require("../../models");
 router.get("/", async (req, res) => {
   try {
     const entryData = await Entry.findAll(req.body);
-    res.status(200).json(habitData);
+    res.status(200).json(entryData);
   } catch (err) {
     res.status(400).json(err);
   }
 });
+
+module.exports = router;
 
 router.post("/", (req, res) => {
   Entry.create(req.body)
--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,8 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
-// const entryRoutes = require("./api/entryRoutes");
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
-// router.use("/entries", entryRoutes);
 
 module.exports = router;
