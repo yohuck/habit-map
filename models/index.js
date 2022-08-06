eecf32541e585ef3a4a18863b5f5acd35480471d
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-08-05 18:11:53 -0600

    commenting out entry requirement

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require("./User");
 const Habit = require("./Habit");
-const Entry = require("./Entry");
+// const Entry = require("./Entry");
 
 User.hasMany(Habit, {
   foreignKey: "user_id",
@@ -11,4 +11,4 @@ Habit.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-module.exports = { User, Habit, Entry };
+module.exports = { User, Habit };
