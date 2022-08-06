27b2946cdfcb39737cde94f8eee5fba4ede4bc5e
Eric Lake <ericallenlake@gmail.com>
2022-08-06 12:47:49 -0600

    Pulls user data through

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
-const signup = document.getElementById('signup')
-const new_habit = document.getElementById('new_habit')
+// const signup = document.getElementById('signup')
+// const new_habit = document.getElementById('new_habit')
 
 // signup.addEventListener('click', () => {
 
