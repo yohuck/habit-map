ee27963700a6cc3f4d6b7d6ba00e43783ba5c433
ledford30 <101572791+Vesuro30@users.noreply.github.com>
2022-08-06 14:18:06 -0600

    entry route work

--- controllers/api/entryRoutes.js
+++ controllers/api/entryRoutes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
+const { BOOLEAN, DataTypes } = require("sequelize");
 const { Entry } = require("../../models");
 
+//  Get all entries
 router.get("/", async (req, res) => {
   try {
     const entryData = await Entry.findAll(req.body);
@@ -12,6 +14,7 @@ router.get("/", async (req, res) => {
 
 module.exports = router;
 
+//  Add an entry
 router.post("/", (req, res) => {
   Entry.create(req.body)
     .then((newEntry) => {
@@ -20,6 +23,60 @@ router.post("/", (req, res) => {
     .catch((err) => {
       res.json(err);
     });
+});
+
+// //  Create an entry 2nd method -- possible
+// router.post("/", (req, res) => {
+//   Entry.create({
+//     id: req.body.id,
+//     date: req.body.date,
+//     completed: BOOLEAN,
+//     habitId: DataTypes.INTEGER,
+//   })
+//     .then((newEntry) => {
+//       res.json(newEntry);
+//     })
+//     .catch((err) => {
+//       res.json(err);
+//     });
+// });
+
+//  Update an entry
+router.put("/:id", async (req, res) => {
+  try {
+    const entryData = await Entry.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!entryData[0]) {
+      res.status(404).json({
+        message: "There is no entry that matches that ID!  Please try again.",
+      });
+      return;
+    }
+    res.status(200).json(entryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//  Delete an entry
+router.delete("/:id", async (req, res) => {
+  try {
+    const entryData = await Entry.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!entryData) {
+      res.status(404).json({ message: "No entry with this id!" });
+      return;
+    }
+    res.status(200).json(entryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
