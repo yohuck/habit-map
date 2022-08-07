56e808f6999edf09765427ef7685d2e435cc23df
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-08-07 15:29:07 -0600

    adding Auth to routes/update & delete

--- controllers/api/habitRoutes.js
+++ controllers/api/habitRoutes.js
@@ -1,57 +1,84 @@
 const router = require("express").Router();
-const { Habit } = require("../../models");
+const { Habit, Entry } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 //getting all habitData
-router.get("/", async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
-    const habitData = await Habit.findAll(req.body);
+    const habitData = await Habit.findAll();
     res.status(200).json(habitData);
-  } catch (err) {
-    res.status(400).json(err);
+  } catch (error) {
+    res.status(400).json({ message: `An ${error} has occured.` });
   }
 });
 
 //getting habitData by id
-router.get("/:id", async (req, res) => {
+router.get("/:id", withAuth, async (req, res) => {
   try {
     const habitData = await Habit.findByPk(req.params.id);
     if (!habitData) {
-      res.status(404).json({ message: "No habit with this id!" });
+      res.status(404).json({ message: "No habit with this id." });
       return;
     }
     res.status(200).json(habitData);
-  } catch (err) {
-    res.status(500).json(err);
+  } catch (error) {
+    res.status(500).json({ message: `An ${error} has occured.` });
   }
 });
 
 //creating a new habit
-router.post("/", async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   try {
-    const newHabit = await Habit.create(req.body);
-    res.status(200).json(newHabit);
-  } catch (err) {
-    res.status(400).json(err);
+    const newHabit = await Habit.create({
+      name: req.body.name,
+      description: req.body.description,
+      userId: req.body.userId,
+    });
+    res.status(200).json({ message: `A new habit was successfully created.` });
+  } catch (error) {
+    res.status(400).json({ message: `An ${error} has occured.` });
+  }
+});
+
+//UPDATE route for a specific Habit id
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const habitData = await Habit.update(
+      {
+        name: req.body.name,
+        description: req.body.description,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!habitData[0]) {
+      res.status(404).json({ message: "No habit was found with this id." });
+      return;
+    }
+    res.status(200).json({ message: `Habit was successfully updated.` });
+  } catch (error) {
+    res.status(500).json({ message: `An ${error} has occured.` });
   }
 });
 
 //DELETE route for a specific Habit id
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", withAuth, async (req, res) => {
   try {
     const habitData = await Habit.destroy({
       where: {
         id: req.params.id,
       },
     });
-
     if (!habitData) {
-      res.status(404).json({ message: "No habit found with that id!" });
+      res.status(404).json({ message: "No habit found with that id." });
       return;
     }
-
-    res.status(200).json(habitData);
-  } catch (err) {
-    res.status(500).json(err);
+    res.status(200).json({ message: `Habit was successfully deleted.` });
+  } catch (error) {
+    res.status(500).json({ message: `An ${error} has occured.` });
   }
 });
 
