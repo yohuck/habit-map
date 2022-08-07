cd5fe387c738f1ed77bbe761547eb7830e161b91
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-08-07 14:44:58 -0600

    Update habitRoutes.js

--- controllers/api/habitRoutes.js
+++ controllers/api/habitRoutes.js
@@ -35,5 +35,24 @@ router.post("/", async (req, res) => {
   }
 });
 
-//need to create the put and delete routes
+//DELETE route for a specific Habit id
+router.delete("/:id", async (req, res) => {
+  try {
+    const habitData = await Habit.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!habitData) {
+      res.status(404).json({ message: "No habit found with that id!" });
+      return;
+    }
+
+    res.status(200).json(habitData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
