1945adc7b11aff90eaaba6e833eaadc0b56db406
ledford30 <101572791+Vesuro30@users.noreply.github.com>
2022-08-08 19:23:20 -0600

    message changes

--- controllers/api/entryRoutes.js
+++ controllers/api/entryRoutes.js
@@ -8,7 +8,7 @@ router.get("/", async (req, res) => {
   try {
     const entryData = await Entry.findAll(req.body);
     res.status(200).json(entryData);
-  } catch (err) {
+  } catch (error) {
     res.status(400).json({ message: `An ${error} has occured.` });
   }
 });
@@ -63,9 +63,9 @@ router.put("/:id", async (req, res) => {
       });
       return;
     }
-    res.status(200).json(entryData);
+    res.status(200).json({ message: `Entry successfully updated.` });
   } catch (error) {
-    res.status(500).json({ message: `An ${error} has occured` });
+    res.status(500).json({ message: `An ${error} has occured.` });
   }
 });
 
@@ -81,9 +81,9 @@ router.delete("/:id", async (req, res) => {
       res.status(404).json({ message: "No entry with this id!" });
       return;
     }
-    res.status(200).json(entryData);
-  } catch (err) {
-    res.status(500).json(err);
+    res.status(200).json({ message: `Entry successfully deleted.` });
+  } catch (error) {
+    res.status(500).json({ message: `An ${error} has occurred.` });
   }
 });
 
