9ee219d72a39da493eb96c2e211d3055eaba71de
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-08-09 15:56:51 -0600

    editing get by id user route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -17,7 +17,9 @@ router.get("/", async (req, res) => {
 //getting userData by id
 router.get("/:id", async (req, res) => {
   try {
-    const userData = await User.findByPk(req.params.id);
+    const userData = await User.findByPk(req.params.id, {
+      include: [{ model: Habit, include: [{ model: Entry }] }],
+    });
     if (!userData) {
       res.status(404).json({ message: "No user with this id!" });
       return;
