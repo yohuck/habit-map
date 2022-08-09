dc25d3b77d6cd17fa1b6528ff6ffeca753f5e48a
Eric Lake <99624222+yohuck@users.noreply.github.com>
2022-08-08 19:30:31 -0600

    Update seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -17,6 +17,10 @@ const seedDatabase = async () => {
   const habits = await Habit.bulkCreate(habitData, {
     returning: true,
   });
+  
+   const entries = await Entry.bulkCreate(entryData, {
+    returning: true,
+  });
 
   process.exit(0);
 };
